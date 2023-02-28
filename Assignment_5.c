6ba90218b855cdb8bb58aaeebb01e250fc45da79
Jay <117885838+jay0011@users.noreply.github.com>
2023-02-28 09:59:47 +0530

    Bisection || Regula-Falsi || Newton-Raphson

--- Assignment_5.c
+++ Assignment_5.c
@@ -8,15 +8,8 @@ float Round(float n){
     return round(n*1000)/1000;
 }
 
-void Bisection(){
-    float a,b,x=0;
+void Bisection(float a,float b){
 
-    while(1){
-        if(f(x)>0) break;
-        x++;
-    }
-    a=x-1;
-    b=x;
 
     while(1){
         if(f((a+b)/2)<0) a=(a+b)/2;
@@ -24,18 +17,11 @@ void Bisection(){
         // printf("%f\t%f\t%f\n",a,b,f((a+b)/2));
         if(Round(a)==Round(b)) break;
     }
-    printf("Bisection = %0.3f\n",(a+b)/2);
+    printf("using Bisection Method = %0.3f\n",(a+b)/2);
 }
 
-void Regula_Falsi(){
-    float a,b,x=0;
-
-    while(1){
-        if(f(x)>0) break;
-        x++;
-    }
-    a=x-1;
-    b=x;
+void Regula_Falsi(float a,float b){
+    float x;
 
     while(1){
         x=(a*f(b)-b*f(a))/(f(b)-f(a));
@@ -45,18 +31,10 @@ void Regula_Falsi(){
         if(Round(a)==Round(b)) break;
     }
 
-    printf("Regula_Falsi = %0.3f\n",a);
+    printf("using Regula-Falsi Method = %0.3f\n",a);
 }
 
-void Newton_Raphson(){
-    float prev,curr,x=0;
-
-    while(1){
-        if(f(x)>0) break;
-        x++;
-    }
-    prev=x-1;
-    curr=x;
+void Newton_Raphson(float prev,float curr){
 
     while(1){
         prev-=f(prev)/d(prev);
@@ -66,14 +44,20 @@ void Newton_Raphson(){
         if(Round(curr)==Round(prev)) break;
         prev=curr;
     }
-    printf("Newton-Raphson = %0.3f\n",curr);
+    printf("using Newton-Raphson Method = %0.3f\n",curr);
 
 }
 
 int main(){
-    
-    Regula_Falsi();
-    Bisection();
-    Newton_Raphson();
+    float x=0;
+
+    while(1){
+        if(f(x)>0) break;
+        x++;
+    }
+
+    Bisection(x-1,x);
+    Regula_Falsi(x-1,x);
+    Newton_Raphson(x-1,x);
 }
 
